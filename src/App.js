aa5a78bb1bfdfa5cda75cc94796f3dba57570c98
Yun,Ning <dackdada016@gmail.com>
2023-01-08 14:59:02 +0800

    react 計數器

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  // 宣告狀態用的語法 [得到狀態, 設定狀態] = 初始值
+  const [total, setTotal] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* 建立事件監聽後(onClick)，再設定事件發生時的狀態*/}
+      <h1
+        onClick={() => {
+          setTotal(total + 1)
+        }}
+      >
+        {total}
+      </h1>
+    </>
+  )
 }
 
-export default App;
+export default App
