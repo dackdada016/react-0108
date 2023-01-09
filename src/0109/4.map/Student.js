9cf7c5eb6ed0e138441bcb7ab597ab25c71e31ad
Yun,Ning <dackdada016@gmail.com>
2023-01-09 14:03:54 +0800

    react map的使用及Key值

---
+++ src/0109/4.map/Student.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    id: 1,
+    name: '小花',
+  },
+  {
+    id: 2,
+    name: '小草',
+  },
+]
+//
+function Student() {
+  return (
+    <>
+      <h1>學生資料範例(map)</h1>
+      <ul>
+        {/* 
+            Key 幫助 React 分辨哪些項目被改變、增加或刪除。在 array 裡面的每個 element 都應該要有一個 key，如此才能給予每個 element 一個固定的身份：
+            key的選擇
+            1.資料來自資料庫，優先使用資料庫的key(主/外鍵)當key值
+            2.資料來源不是資料庫，可在初始化時使用uuid或nanoid等函式庫產生key值，不能寫在元件的return(render)中
+            3.資料為靜態資料(執行時完全不會更動時),才能使用索引值當作key
+         */}
+        {data.map((v, i) => {
+          return <li key={v.id}>{v.name}</li>
+        })}
+      </ul>
+    </>
+  )
+}
+export default Student
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import Counter from './0109/1-counter/Counter'
 // import JSXValue from './0109/2-JSXValue/JSXValue'
-import JSXValue2 from './0109/3.jsx/JSXValue2'
+// import JSXValue2 from './0109/3.jsx/JSXValue2'
+import Student from './0109/4.map/Student'
 function App() {
   return (
     <>
-      <JSXValue2 />
+      <Student />
     </>
   )
 }
