a4fc5f013c2f47e5a4617cd2ae41347b2cd61217
Yun,Ning <dackdada016@gmail.com>
2023-01-09 11:33:25 +0800

    計數器

---
+++ src/0109/1-counter/Counter.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+function Counter() {
+  // 宣告狀態用的語法 [得到狀態, 設定狀態] = 使用狀態(初始值)
+  const [total, setTotal] = useState(0)
+  return (
+    <>
+      {/* 建立事件監聽後(onClick)，再設定事件發生時的狀態*/}
+      <h1
+        onClick={() => {
+          setTotal(total + 1)
+        }}
+      >
+        {total}
+      </h1>
+    </>
+  )
+}
+
+export default Counter
