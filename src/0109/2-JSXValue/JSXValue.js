a671c8b1b0dc08969a80968aa99f0dde611a2fac
Yun,Ning <dackdada016@gmail.com>
2023-01-09 11:36:07 +0800

    元件引用css樣式

---
+++ src/0109/2-JSXValue/JSXValue.js
@@ -0,0 +1,23 @@
+import './JSXValue.css'
+
+function JSXValue() {
+  return (
+    <>
+      <div class="card card-text">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default JSXValue
--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Counter from './0109/1-counter/Counter'
-
+import JSXValue from './0109/2-JSXValue/JSXValue'
 function App() {
   return (
     <>
       <Counter />
+      <JSXValue />
     </>
   )
 }
