eb83ba77d93efe32dbb60b48062d22c9449aedd3
Yun,Ning <dackdada016@gmail.com>
2023-01-10 11:22:36 +0800

    props 子元件資料傳遞給父元件 (call back function)

--- src/0110/3.props-2/ChildB.js
+++ src/0110/3.props-2/ChildB.js
@@ -1,7 +1,12 @@
+import { useState } from 'react'
 function ChildB(props) {
+  // 子元件中的內部資料
+  const [cData, setCData] = useState('child B data')
   return (
     <>
       <h1>ChildB</h1>
+      {/* 利用父元件props裡設定state的函式，傳值給回父元件 */}
+      <button onClick={props.setDataFromChild(cData)}>傳送資料給父元件</button>
     </>
   )
 }
--- src/0110/3.props-2/Parent.js
+++ src/0110/3.props-2/Parent.js
@@ -5,12 +5,17 @@ import ChildB from './ChildB'
 function Parent() {
   // 父元件的資料
   const [pData, setPData] = useState('parent data')
+
+  // 準備接收子元件的資料
+  const [dataFromChid, setDataFromChild] = useState('')
   return (
     <>
       <h1>Parent</h1>
 
       <ChildA pData={pData} />
-      <ChildB />
+      {/* 給子元件B可以設定state的函式 */}
+      <ChildB setDataFromChild={setDataFromChild} />
+      <p>來自子元件B的資料 {dataFromChid} </p>
     </>
   )
 }
