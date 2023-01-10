82c31a4d9a7a0bb0a0abf4ffc4f7fd651c65080c
Yun,Ning <dackdada016@gmail.com>
2023-01-10 11:32:37 +0800

    建議解構的方式

--- src/0110/2.props-1/Child.js
+++ src/0110/2.props-1/Child.js
@@ -2,15 +2,18 @@
 // 1. 傳入參數時直接解構 function Child({ text, color, foo })
 // 2. 傳入之後在解構 const { text, color, foo } = props
 
-function Child(props) {
-  // { text, color, foo }
-
-  const { text, color, foo } = props
+function Child({
+  text = 'hello',
+  color = 'red',
+  foo = () => {
+    alert('test')
+  },
+}) {
   return (
     <>
       文字:{text}
       顏色:{color}
-      <button onClick={props.foo}>Try</button>
+      <button onClick={foo}>Try</button>
     </>
   )
 }
