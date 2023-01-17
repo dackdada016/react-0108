7e399b594e6ca56f6acc251b91f70e9d881367f7
Yun,Ning <dackdada016@gmail.com>
2023-01-17 16:36:58 +0800

    1.useRef 完全模擬didUpdate 2.useEffect 模擬willUnmount

--- src/0117/2.lifecyde/FC.js
+++ src/0117/2.lifecyde/FC.js
@@ -1,7 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 function FC() {
   const [total, setTotal] = useState(0)
+
+  // 紀錄進入didMount時間點的內部變數
+  const didMountRef = useRef(false)
+
   //useEffect 只能寫在函式裡，不能寫在 if 或 return裡面
   //模擬 didMount
   useEffect(() => {
@@ -17,6 +21,30 @@ function FC() {
   }, [total])
   // ^^^^^^^ 相依性陣列中，用於放入與更新階段有關的props(從父母原件來的)或state(自己本身的)
   // 意義是當放入的state或props變更時，執行第一參數函式中的程式碼(行為模式相似於onChange)，才會觸發更新(update/re-render)階段
+
+  // 只模擬 didUpdate
+  useEffect(() => {
+    // 略過初始值
+    if (total !== 0) {
+      console.log('模擬DidUpdate', 'total=', total)
+    }
+  }, [total])
+
+  // 完全模擬didUpdate
+  useEffect(() => {
+    if (didMountRef.current) {
+      console.log('模擬didUpdate-3', 'total=', total)
+    } else {
+      didMountRef.current = true
+    }
+  }, [total])
+
+  // 模擬willUnmount
+  useEffect(() => {
+    return () => {
+      console.log('willUnmount')
+    }
+  }, [])
 
   return (
     <>
